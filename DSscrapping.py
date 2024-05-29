f15bf50e601facb37d85d8c7283f24acdfcdd19d
naylaharyadi <haryadinayla@outlook.Com>
2024-05-29 14:37:11 +0200

    2pr pages et tag books

--- DSscrapping.py
+++ DSscrapping.py
@@ -25,4 +25,40 @@ session, token = login_and_get_token()
 with open('results.csv', 'w') as file:
     file.write(f"token\n{token}\n")
 print("Token has been written to 'results.csv'.")
-# Function to scrape quotes with a specific tag
+
+def scrape_tagged_quotes(session, tag, pages=2):
+    base_url = f"https://quotes.toscrape.com/tag/{tag}/page/{{}}/"
+    quotes = []
+    for page_num in range(1, pages + 1):
+        url = base_url.format(page_num)
+        response = session.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        for quote in soup.select('div.quote'):
+            text = quote.find('span', class_='text').get_text(strip=True)
+            author = quote.find('small', class_='author').get_text(strip=True)
+            tags = [tag.get_text(strip=True) for tag in quote.find_all('a', class_='tag')]
+            quotes.append({
+                'text': text,
+                'author': author,
+                'tags': tags
+            })
+    return quotes
+# Scrape the first 2 pages of quotes with the 'books' tag
+tag = 'books'
+book_quotes = scrape_tagged_quotes(session, tag, pages=2)
+# Function to filter out duplicate quotes
+def filter_duplicates(quotes):
+    seen = set()
+    filtered_quotes = []
+    for quote in quotes:
+        text = quote['text']
+        if text not in seen:
+            seen.add(text)
+            filtered_quotes.append(quote)
+    return filtered_quotes
+# Filter out duplicate quotes
+unique_book_quotes = filter_duplicates(book_quotes)
+# Append quotes to results.csv
+df_quotes = pd.DataFrame(unique_book_quotes, columns=['text', 'author', 'tags'])
+df_quotes.to_csv('results.csv', mode='a', index=False, header=True)
+print("Unique book quotes have been appended to 'results.csv'.")
