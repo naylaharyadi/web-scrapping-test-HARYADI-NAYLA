ca98d16fc62dc38df7785e36a2a489afb593c2ac
naylaharyadi <haryadinayla@outlook.Com>
2024-05-29 13:53:20 +0200

    fichier csv

--- DSscrapping.py
+++ DSscrapping.py
@@ -1,31 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-def scrape_quotes():
+def scrape_filtered_quotes():
     base_url = "https://quotes.toscrape.com/page/{}/"
-    selected_tags = {"love", "inspirational", "life", "humor"}
+    selected_tags = {'love', 'inspirational', 'life', 'humor'}
     all_quotes = []
 
-    for page in range(1, 6):  # Scrape les 5 premières pages
-        url = base_url.format(page)
-        response = requests.get(url)
+    for page in range(1, 6):  
+        response = requests.get(base_url.format(page))
         soup = BeautifulSoup(response.text, 'html.parser')
-
+        
         quotes = soup.find_all('div', class_='quote')
         
         for quote in quotes:
             text = quote.find('span', class_='text').text
             tags = {tag.text for tag in quote.find_all('a', class_='tag')}
-            
-            # Filtrer pour ne garder que les citations ayant des tags sélectionnés
-            if tags & selected_tags:
-                all_quotes.append({'quote': text, 'tags': list(tags & selected_tags)})
+            if tags & selected_tags:  
+                all_quotes.append({'text': text, 'tags': ', '.join(tags & selected_tags)})
 
-    return all_quotes
+    # écrire dans un fichier CSV
+    df = pd.DataFrame(all_quotes)
+    df.to_csv('results.csv', index=False)
 
-# Exécutez cette fonction et affichez les résultats dans un tableau
-quotes_data = scrape_quotes()
-for quote in quotes_data:
-    print(quote)
-
-
+scrape_filtered_quotes()
